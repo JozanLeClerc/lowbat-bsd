34f348f45b3c113433176e7553b5074aad1d5c37
JozanLeClerc <bousset.rudy@gmail.com>
2020-04-18 15:20:23 +0200

    eSpeak is the problem

--- asm/jo_r_loop.asm
+++ asm/jo_r_loop.asm
@@ -14,26 +14,21 @@
 ;; -------------------------------
 
 section .text
-	extern puts
 	extern jo_n_speak
+	extern jo_n_notify
 	global jo_r_loop
 
 jo_r_loop:
-	push	rcx
+	push	rbp
 	push	rbx
 	mov		rbx, rsi
-	mov		rcx, rdi
+	mov		ebp, edi
 
 speak1:
-	cmp		rcx, 0x1
-	jne		speak1
+	;; cmp		edx, 1
+	;; jne		speak1
 	mov		rdi, [rbx + 8 * 2]
 	call	jo_n_speak
-	pop		rcx
-	push	rdi
-	mov		rdi, message
-	call	puts
-	pop		rdi
 	jmp		speak1
 
 return:
@@ -41,4 +36,4 @@ return:
 	retq
 
 section .data
-	message:	db "qwe", 0x0
+	msg:	db "qwe", 0x0
