c57228e66ab9754c78b63e19153439276890c0c8
JozanLeClerc <bousset.rudy@gmail.com>
2020-04-27 02:14:31 +0200

    Removed old version

--- asm/c_args.asm
+++ asm/c_args.asm
@@ -57,34 +57,3 @@ return:
 
 .data
 	reference:	.asciz "--say"
-
-/* ;; section .text */
-/* ;; 	extern strncmp */
-/* ;; 	global jo_c_args */
-/*  */
-/* ;; jo_c_args: */
-/* ;; 	cmp		rdi, 0x2 */
-	/* ;; jle		no_args */
-	/* ;; mov		rax, [rsi + 0x8 * 0x2] */
-	/* ;; cmp		byte [rax + 0x0], 0x0 */
-	/* ;; je		no_args */
-	/* ;; push	rdi */
-	/* ;; mov		rdi, [rsi + 0x8 * 0x1] */
-	/* ;; push	rsi */
-	/* ;; mov		rsi, reference */
-	/* ;; mov		rdx, reflen */
-	/* ;; call	strncmp */
-	/* ;; pop		rsi */
-	/* ;; pop		rdi */
-	/* ;; cmp		rax, 0x0 */
-	/* ;; jne		no_args */
-	/* ;; mov		rax, 0x1 */
-	/* ;; retq */
-/*  */
-/* ;; no_args: */
-/* ;; 	xor		rax, rax */
-/* ;; 	retq */
-/*  */
-/* ;; section .data */
-/* ;; 	reference:	db "--say", 0x0 */
-/* ;; 	reflen:		equ $ - reference */
